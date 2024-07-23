6f48e8273ea0c32d24d927d59da47b4c3dfd18eb
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-07-24 00:55:29 +0500

    db configured and user table created

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+
+const createUserTable = async (db) => {
+  await db.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+  await db.query(`CREATE TABLE IF NOT EXISTS users(
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+)`);
+};
+
+const main = async () => {
+  const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+  });
+
+  const db = await pool.connect();
+
+  await createUserTable(db);
+
+  db.release();
+};
+
+main()
+  .then(() => console.log('Tables created successfully 🎉'))
+  .catch((error) => error);
